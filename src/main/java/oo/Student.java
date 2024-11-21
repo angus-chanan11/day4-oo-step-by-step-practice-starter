2df74a3f386bc88646d7daa4b9d1e127e0b8f955
angus-chanan11 <angus.chan@oocl.com>
2024-11-21 15:11:34 +0800

    [OO][Practice] refactor: remove magic string and store as CLASS_MESSAGE_END

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -6,6 +6,7 @@ public class Student extends Person{
     private static final String LEADER_MESSAGE = "I am the leader of class ";
     private static final String LEADER_MESSAGE_END = ".";
     private static final String CLASS_MESSAGE = "I am in class ";
+    private static final String CLASS_MESSAGE_END = ".";
 
     public Student(Integer id, String name, Integer age) {
         super(id, name, age);
@@ -20,7 +21,7 @@ public class Student extends Person{
             if (klass.isLeader(this)){
                 builder.append(String.format(" %s%d%s", LEADER_MESSAGE, klassNumber, LEADER_MESSAGE_END));
             } else {
-                builder.append(String.format(" %s%d.", CLASS_MESSAGE, klassNumber));
+                builder.append(String.format(" %s%d%s", CLASS_MESSAGE, klassNumber, CLASS_MESSAGE_END));
             }
         }
         return builder.toString();
