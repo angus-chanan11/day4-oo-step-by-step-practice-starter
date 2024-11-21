6e13fe13a922373d8d28f7d267d94f4c9723e76e
angus-chanan11 <angus.chan@oocl.com>
2024-11-21 14:16:00 +0800

    [OO][Practice] feat: add Klass with check number equal

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,4 +1,24 @@
 package oo;
 
+import java.util.Objects;
+
 public class Klass {
+    private Integer number;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object anotherObject) {
+        if (anotherObject instanceof Klass anotherKlass)
+            return number.equals(anotherKlass.number);
+        else
+            return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 }
