84b048bde26f69f558387244b286ffb48fe8b232
angus-chanan11 <angus.chan@oocl.com>
2024-11-21 14:32:54 +0800

    [OO][Practice] feat: add belongsTo for Teacher

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,12 +1,22 @@
 package oo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Teacher extends Person{
+    private List<Klass> klasses;
+
     public Teacher(Integer id, String name, Integer age) {
         super(id, name, age);
+        klasses = new ArrayList<>();
     }
 
     @Override
     public String introduce() {
         return String.format("%s I am a teacher.", super.introduce());
+    }
+
+    public boolean belongsTo(Klass klass) {
+        return klasses.contains(klass);
     }
 }
