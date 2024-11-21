d5166170bd8bd0577686a183097cbb164b603b08
angus-chanan11 <angus.chan@oocl.com>
2024-11-21 13:58:00 +0800

    [OO][Practice] feat: add student class and introduce

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -4,8 +4,8 @@ import java.util.Objects;
 
 public class Person {
     private int id;
-    private String name;
-    private int age;
+    protected String name;
+    protected int age;
 
     public Person(int id, String name, int age) {
         this.id = id;
--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,4 +1,12 @@
 package oo;
 
-public class Student {
+public class Student extends Person{
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a student.", name, age);
+    }
 }
