f7b473b3f014988adf4b8a4fcbb94250559e28af
angus-chanan11 <angus.chan@oocl.com>
2024-11-21 14:01:07 +0800

    [OO][Practice] feat: add teacher class and introduce

--- src/test/java/oo/step2/TeacherTest.java
+++ src/test/java/oo/step2/TeacherTest.java
@@ -7,21 +7,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TeacherTest {
 
-//    @Test
-//    public void should_return_message_with_name_and_age_when_introduce_given_teacher() {
-//        Teacher jerry = new Teacher(1, "Jerry", 21);
-//        String introduce = jerry.introduce();
-//
-//        assertThat(introduce).isEqualTo("My name is Jerry. I am 21 years old. I am a teacher.");
-//    }
-//
-//    @Test
-//    public void should_return_message_with_name_and_age_when_introduce_given_another_teacher() {
-//        Teacher robert = new Teacher(1, "Robert", 20);
-//        String introduce = robert.introduce();
-//
-//        assertThat(introduce).isEqualTo("My name is Robert. I am 20 years old. I am a teacher.");
-//    }
+    @Test
+    public void should_return_message_with_name_and_age_when_introduce_given_teacher() {
+        Teacher jerry = new Teacher(1, "Jerry", 21);
+        String introduce = jerry.introduce();
+
+        assertThat(introduce).isEqualTo("My name is Jerry. I am 21 years old. I am a teacher.");
+    }
+
+    @Test
+    public void should_return_message_with_name_and_age_when_introduce_given_another_teacher() {
+        Teacher robert = new Teacher(1, "Robert", 20);
+        String introduce = robert.introduce();
+
+        assertThat(introduce).isEqualTo("My name is Robert. I am 20 years old. I am a teacher.");
+    }
 //
 //    @Test
 //    public void should_return_true_when_equals_given_two_teachers_with_same_id() {
