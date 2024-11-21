77bc6e77eff46557333496283beb00a21664de75
angus-chanan11 <angus.chan@oocl.com>
2024-11-21 16:27:43 +0800

    [OO][Practice] refactor: combine constant

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -4,9 +4,8 @@ public class Student extends Person{
     private Klass klass;
     private static final String STUDENT_MESSAGE = "I am a student.";
     private static final String LEADER_MESSAGE = "I am the leader of class ";
-    private static final String LEADER_MESSAGE_END = ".";
+    private static final String MESSAGE_END = ".";
     private static final String CLASS_MESSAGE = "I am in class ";
-    private static final String CLASS_MESSAGE_END = ".";
 
     public Student(Integer id, String name, Integer age) {
         super(id, name, age);
@@ -19,9 +18,9 @@ public class Student extends Person{
         if (klass != null) {
             Integer klassNumber = klass.getNumber();
             if (klass.isLeader(this)){
-                builder.append(String.format(" %s%d%s", LEADER_MESSAGE, klassNumber, LEADER_MESSAGE_END));
+                builder.append(String.format(" %s%d%s", LEADER_MESSAGE, klassNumber, MESSAGE_END));
             } else {
-                builder.append(String.format(" %s%d%s", CLASS_MESSAGE, klassNumber, CLASS_MESSAGE_END));
+                builder.append(String.format(" %s%d%s", CLASS_MESSAGE, klassNumber, MESSAGE_END));
             }
         }
         return builder.toString();
